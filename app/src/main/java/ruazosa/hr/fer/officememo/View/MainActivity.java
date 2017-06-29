0f68604d03cfdfd7427f1386ed12924a7f530608
Shimun Matić <shimun.matic@gmail.com>
2017-06-29 16:29:29 +0200

    Back to good ol Java

---
+++ app/src/main/java/ruazosa/hr/fer/officememo/View/MainActivity.java
@@ -0,0 +1,17 @@
+package ruazosa.hr.fer.officememo.View;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import ruazosa.hr.fer.officememo.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
