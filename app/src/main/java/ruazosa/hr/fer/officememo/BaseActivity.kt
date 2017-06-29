7b2d2eeaa02d6eba5f94f2aec63fa6d9f535d8ac
Matija Vukić <matija.vukic@fer.hr>
2017-06-30 01:37:07 +0200

    commit

---
+++ app/src/main/java/ruazosa/hr/fer/officememo/BaseActivity.kt
@@ -0,0 +1,15 @@
+package ruazosa.hr.fer.officememo
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * Created by matija on 30.06.17..
+ */
+open class BaseActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+    }
+}
--- app/src/main/java/ruazosa/hr/fer/officememo/View/MainActivity.java
+++ app/src/main/java/ruazosa/hr/fer/officememo/View/MainActivity.java
@@ -4,9 +4,10 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import ruazosa.hr.fer.officememo.BaseActivity;
 import ruazosa.hr.fer.officememo.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
